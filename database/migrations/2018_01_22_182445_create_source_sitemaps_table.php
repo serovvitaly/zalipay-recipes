84fd1472c5f8131359a9256d1d3db9912fa484cb
vi <serovvitaly@gmail.com>
2018-01-22 22:21:49 +0300

    Alter table source_sitemaps

--- database/migrations/2018_01_22_182445_create_source_sitemaps_table.php
+++ database/migrations/2018_01_22_182445_create_source_sitemaps_table.php
@@ -17,6 +17,7 @@ class CreateSourceSitemapsTable extends Migration
             $table->increments('id');
             $table->integer('source_id');
             $table->string('sitemap_url')->unique();
+            $table->boolean('is_active')->default(true);
         });
     }
 
