3432047c49d1d2ddb4e66bf3a9a78acbb6b225ce
vi <serovvitaly@gmail.com>
2018-01-24 10:23:50 +0300

    Миграция для рецептов

---
+++ database/migrations/2018_01_23_211511_create_recipes_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('url_id');
+            $table->string('name');
+            $table->string('image');
+            $table->string('recipe_yield');
+            $table->string('total_time');
+            $table->string('recipe_cuisine');
+            $table->string('description');
+        });
+
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('recipe_id');
+            $table->string('name');
+            $table->string('value');
+        });
+
+        Schema::create('recipe_instructions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('recipe_id');
+            $table->integer('step_num');
+            $table->text('text');
+            $table->string('media_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('recipe_ingredients');
+        Schema::dropIfExists('recipe_instructions');
+    }
+}
